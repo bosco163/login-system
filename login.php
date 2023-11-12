f21b8c5dd0018a5ee2dbb52d4573cdba420394d2
Bosco <121373761+bosco163@users.noreply.github.com>
2023-11-12 09:47:27 +0800

    Update login.php

--- login.php
+++ login.php
@@ -19,23 +19,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo "Username or password errors ! ";
 }
 ?>
-
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-
-    $users = file("users.txt", FILE_IGNORE_NEW_LINES);
-
-    foreach ($users as $user) {
-        list($savedUsername, $savedPassword) = explode("|", $user);
-
-        if ($username === $savedUsername && password_verify($password, $savedPassword)) {
-            echo "<h1>Welcome ! </h1>";
-            echo "Now time : " . date("Y-m-d H:i:s");
-            exit;
-        }
-    }
-
-}
-?>
